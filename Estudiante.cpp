bb6b9bb80d39dd88a5a606d3d23e27e7031300e5
Bárbara Torres Sulca <btorrsul@myuax.com>
2024-11-21 12:05:45 +0100

    Estudiante

---
+++ Estudiante.cpp
@@ -0,0 +1,5 @@
+//
+// Created by barba on 21/11/2024.
+//
+
+#include "Estudiante.h"
---
+++ Estudiante.h
@@ -0,0 +1,8 @@
+//
+// Created by barba on 21/11/2024.
+//
+
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
+
+#endif //ESTUDIANTE_H
