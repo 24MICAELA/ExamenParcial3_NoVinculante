e96ec11f556e99ea060de5acb2e6fca73bce4e6a
Bárbara Torres Sulca <btorrsul@myuax.com>
2024-11-22 10:14:55 +0100

    cpp

--- Asistencia.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Asistencia.h"
-
-Asistencia::Asistencia(const std::string& fecha, const std::string& materia, const std::string& estado)
-    : fecha(fecha), materia(materia), estado(estado) {}
-
-void Asistencia::mostrarAsistencia() const {
-    std::cout << "Fecha: " << fecha << " | Materia: " << materia << " | Estado: " << estado << std::endl;
-}
